34527a1671fc9718387aa2285d14fddfb8f31616
winged-flying-lizard-thing <138835464+winged-flying-lizard-thing@users.noreply.github.com>
2023-07-07 00:31:13 -0400

    Add files via upload

---
+++ members.js
@@ -0,0 +1,52 @@
+export const members = [
+  {
+    "name": "Bearkirb314🐻‍❄️",
+    "profile": "https://www.khanacademy.org/profile/kaid_375460112550893828689953",
+    "level": "Advanced"
+  },
+  {
+    "name": "Voldemort | Lurking",
+    "profile": "https://www.khanacademy.org/profile/kaid_109088529962766375028969",
+    "level": "Advanced"
+  },
+  {
+    "name": "AGnawKneeMouseCoder",
+    "profile": "https://www.khanacademy.org/profile/kaid_9033829293805621369025816",
+    "level": "Intermediate"
+  },
+  {
+    "name": "Zenghao the chicken My Dear Sir",
+    "profile": "https://www.khanacademy.org/profile/kaid_833091817117822871793323",
+    "level": "Intermediate"
+  },
+  {
+    "name": "cloudy skies",
+    "profile": "https://www.khanacademy.org/profile/kaid_880912573786397212785584",
+    "level": "Intermediate"
+  },
+  {
+    "name": "KBraptureReady",
+    "profile": "https://www.khanacademy.org/profile/kaid_499237362224374923964379",
+    "level": "Intermediate"
+  },
+  {
+    "name": "Animator101",
+    "profile": "https://www.khanacademy.org/profile/kaid_457601383838709034574070",
+    "level": "Intermediate"
+  },
+  {
+    "name": "❄️ 𝕃ᴀv Ξ ndΞRGᴀłᴀxʏ🪽",
+    "profile": "https://www.khanacademy.org/profile/kaid_811177991334006852288",
+    "level": "Beginner"
+  },
+  {
+    "name": "TheBlueWyvern",
+    "profile": "https://www.khanacademy.org/profile/kaid_4837281888887525813234462",
+    "level": "Beginner"
+  },
+  {
+    "name": "Pickleprincess",
+    "profile": "https://www.khanacademy.org/profile/kaid_532697917562210154344506",
+    "level": "Beginner"
+  }
+];
---
+++ standings.js
@@ -0,0 +1,26 @@
+export const standings = [
+  {
+    "team": "ByteBots",
+    "points": 0,
+    "icon": "",
+    "homepage": ""
+  },
+  {
+    "team": "CodeNinjas",
+    "points": 0,
+    "icon": "",
+    "homepage": ""
+  },
+  {
+    "team": "HackaHawks",
+    "points": 0,
+    "icon": "",
+    "homepage": ""
+  },
+  {
+    "team": "ProgramPanthers",
+    "points": 0,
+    "icon": "",
+    "homepage": ""
+  }
+];
