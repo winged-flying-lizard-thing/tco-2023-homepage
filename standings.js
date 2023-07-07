61bed99d03125a68b219cb83256a5db899690417
winged-flying-lizard-thing <138835464+winged-flying-lizard-thing@users.noreply.github.com>
2023-07-07 17:48:56 -0400

    Add placeholder team logos

--- standings.js
+++ standings.js
@@ -2,25 +2,25 @@ const standings = [
   {
     "team": "ByteBots",
     "points": 0,
-    "logo": "",
+    "logo": "https://www.khanacademy.org/computer-programming/new-program/4900638627250176/4871312624107520.png",
     "homepage": ""
   },
   {
     "team": "CodeNinjas",
     "points": 0,
-    "logo": "",
+    "logo": "https://www.khanacademy.org/computer-programming/code-ninjas/6077864416690176/4762935482957824.png",
     "homepage": ""
   },
   {
     "team": "HackaHawks",
     "points": 0,
-    "logo": "",
+    "logo": "https://www.khanacademy.org/computer-programming/hackahawks/5441938405507072/5766394931888128.png",
     "homepage": ""
   },
   {
     "team": "ProgramPanthers",
     "points": 0,
-    "logo": "",
+    "logo": "https://www.khanacademy.org/computer-programming/new-program/5033383902887936/4844842925211648.png",
     "homepage": ""
   }
 ];
